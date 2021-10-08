bf67bcfce5fca13bbc3f3ef14ef0279087f99155
Jamilson do Nascimento <iero.eng.agricola@gmail.com>
2021-10-08 09:25:36 -0300

    middleware adicionado

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-const { response, request } = require("express")
 const express = require("express")
 const {v4: uuidv4} = require("uuid") // yarn add uuid
 
@@ -7,6 +6,19 @@ const app =express()
 app.use(express.json())
 
 const customers = []
+
+//Middleware
+function verifyIfExistsAccountCPF(request, response, next){
+    const {cpf} = request.headers
+    const customer = customers.find((customer)=> customer.cpf === cpf)
+
+    if(!customer){
+        return response.status(400).json({error:" Customer not found!"})
+    }
+    request.customer = customer
+
+    return next()
+}
 
 app.post("/account",(request, response)=> {
     const {cpf, name} =request.body
@@ -29,12 +41,9 @@ app.post("/account",(request, response)=> {
     return response.status(201).send()   
 })
 
-app.get("/statement/", (request, response)=>{
-    const {cpf} = request.headers
-    const customer = customers.find((customer) => customer.cpf === cpf)
-    if(!customer){
-        return response.status(400).json({error:"Customer not found!"})
-    }
+app.get("/statement/",verifyIfExistsAccountCPF, (request, response)=>{
+
+    const { customer} = request
     return response.json(customer.statement)
 
 })
